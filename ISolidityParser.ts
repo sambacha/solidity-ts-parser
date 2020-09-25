d2143961c6f536447555bbea044aaf893fb44729
sam bacha <sam@freighttrust.com>
2020-09-25 02:15:57 -0700

    Update ISolidityParser.ts

--- ISolidityParser.ts
+++ ISolidityParser.ts
@@ -5,7 +5,7 @@ import { CommonTokenStream } from "./CommonTokenStream";
 import { SolidityParser } from "./SolidityParser";
 import { SolidityLexer } from "./SolidityLexer";
 
-const inputStream = new InputStream("<__INPUT__>");
+const inputStream = new InputStream("./test/test.sol");
 const lexer = new SolidityLexer(inputStream);
 const tokenStream = new CommonTokenStream(lexer);
 const parser = new SolidityParser(tokenStream);
